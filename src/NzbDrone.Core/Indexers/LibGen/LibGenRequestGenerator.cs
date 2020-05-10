6eb98118d42ab7594ff84f5861c75c1c7408757e
tingtom <tom_mathews@hotmail.co.uk>
2020-05-10 02:33:18 +0100

    Add LibGen url to request generator so test passes

--- src/NzbDrone.Core/Indexers/LibGen/LibGenRequestGenerator.cs
+++ src/NzbDrone.Core/Indexers/LibGen/LibGenRequestGenerator.cs
@@ -5,6 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using NLog.LayoutRenderers.Wrappers;
+using NzbDrone.Common.Http;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
 
@@ -14,7 +15,11 @@ namespace NzbDrone.Core.Indexers.LibGen
     {
         public IndexerPageableRequestChain GetRecentRequests()
         {
-            return new IndexerPageableRequestChain();
+            var chain = new IndexerPageableRequestChain();
+
+            chain.Add(GetRequest());
+
+            return chain;
         }
 
         public IndexerPageableRequestChain GetSearchRequests(AlbumSearchCriteria searchCriteria)
@@ -25,6 +30,14 @@ namespace NzbDrone.Core.Indexers.LibGen
         public IndexerPageableRequestChain GetSearchRequests(ArtistSearchCriteria searchCriteria)
         {
             return new IndexerPageableRequestChain();
+        }
+
+        private IEnumerable<IndexerRequest> GetRequest()
+        {
+            //TODO: Add search parameters
+            var request = new IndexerRequest($"http://gen.lib.rus.ec/fiction/?q=C%C3%A1sate+Conmigo&criteria=&language=&format=", HttpAccept.Json);
+
+            yield return request;
         }
     }
 }
