a7f1426b4e475de100006b0a76af53841aef977c
tingtom <tom_mathews@hotmail.co.uk>
2020-05-15 23:16:34 +0100

    Download the file locally and pin if setting is set

--- src/NzbDrone.Core/Download/Clients/IPFS/IPFS.cs
+++ src/NzbDrone.Core/Download/Clients/IPFS/IPFS.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentMigrator.Builders.Create.ForeignKey;
 using FluentValidation.Results;
 using Ipfs.Http;
 using NLog;
 using NzbDrone.Common.Disk;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Organizer;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.RemotePathMappings;
 
@@ -28,19 +31,80 @@ namespace NzbDrone.Core.Download.Clients.IPFS
 
         public override DownloadProtocol Protocol => DownloadProtocol.IPFS;
 
+        public string GetDownloadClientId(string filename)
+        {
+            return Definition.Name + "_" + Path.GetFileName(filename) + "_" + _diskProvider.FileGetLastWrite(filename).Ticks;
+        }
+
         public override string Download(RemoteAlbum remoteAlbum)
         {
-            throw new NotImplementedException();
+            IpfsClient client = new IpfsClient(Settings.IPFSNodeUrl);
+
+            var path = Path.Combine(Settings.IPFSDownloadPath, FileNameBuilder.CleanFileName(remoteAlbum.Release.Title) + ".ipfs");
+
+            var task = Task.Run(() =>
+            {
+                // Pin the hash if set in the settings
+                if (Settings.PinHash)
+                {
+                    client.Pin.AddAsync(remoteAlbum.Release.DownloadUrl, false).Wait();
+                }
+
+                // Download the file and store locally
+                using (var file = File.Create(path))
+                {
+                    var stream = client.FileSystem.ReadFileAsync(remoteAlbum.Release.DownloadUrl).Result;
+                    stream.CopyTo(file);
+                }
+            });
+
+            return GetDownloadClientId(path);
         }
 
         public override IEnumerable<DownloadClientItem> GetItems()
         {
-            throw new NotImplementedException();
+            foreach (var file in _diskProvider.GetFiles(Settings.IPFSDownloadPath, SearchOption.TopDirectoryOnly))
+            {
+                if (Path.GetExtension(file) != ".ipfs")
+                {
+                    continue;
+                }
+
+                var title = FileNameBuilder.CleanFileName(Path.GetFileName(file));
+
+                var historyItem = new DownloadClientItem
+                {
+                    DownloadClient = Definition.Name,
+                    DownloadId = GetDownloadClientId(file),
+                    Title = title,
+
+                    CanBeRemoved = true,
+                    CanMoveFiles = true,
+
+                    TotalSize = _diskProvider.GetFileSize(file),
+
+                    OutputPath = new OsPath(file)
+                };
+
+                if (_diskProvider.IsFileLocked(file))
+                {
+                    historyItem.Status = DownloadItemStatus.Downloading;
+                }
+                else
+                {
+                    historyItem.Status = DownloadItemStatus.Completed;
+                }
+
+                yield return historyItem;
+            }
         }
 
         public override DownloadClientInfo GetStatus()
         {
-            throw new NotImplementedException();
+            return new DownloadClientInfo()
+            {
+                IsLocalhost = true
+            };
         }
 
         public override void RemoveItem(string downloadId, bool deleteData)
--- src/NzbDrone.Core/Download/Clients/IPFS/IPFSSettings.cs
+++ src/NzbDrone.Core/Download/Clients/IPFS/IPFSSettings.cs
@@ -22,8 +22,19 @@ namespace NzbDrone.Core.Download.Clients.IPFS
     {
         private static readonly IPFSSettingsValidator Validator = new IPFSSettingsValidator();
 
+        public IPFSSettings()
+        {
+            PinHash = true;
+        }
+
         [FieldDefinition(0, Label = "IPFS Node", Type = FieldType.Url, HelpText = "Url to the IPFS node to use, e.g. http://localhost:5001")]
         public string IPFSNodeUrl { get; set; }
+
+        [FieldDefinition(1, Label = "Download Path", Type = FieldType.Path, HelpText = "")]
+        public string IPFSDownloadPath { get; set; }
+
+        [FieldDefinition(1, Label = "Pin hash", Type = FieldType.Checkbox, HelpText = "Pin the file to your IPFS node", Advanced = true)]
+        public bool PinHash { get; set; }
 
         public NzbDroneValidationResult Validate()
         {
